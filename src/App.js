f0b6009b937b8555eb740e6ce20f100e8d3b0db2
EdgarJeremy <edgarpontoh3141@gmail.com>
2018-10-17 17:42:31 +0800

    + basic layout

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/app.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="whole-container">
+				
+				<div className="menu-container">
+					<div className="routes card">
+					</div>
+					<div className="models card">
+					</div>
+				</div>
+
+				<div className="control-container">
+					<div className="content card">
+					</div>
+				</div>
+
+			</div>
+		);
+	}
 }
 
 export default App;
